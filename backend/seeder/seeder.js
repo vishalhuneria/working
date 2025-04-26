0b5ba50037d47b084f0427af79bfdc47bfab4815
vishalhuneria <vishalhuneria@gmail.com>
2025-04-26 06:53:32 +0530

    last commit

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -4,7 +4,9 @@ import Product from "../models/product.js";
 
 const seedProducts = async () => {
   try {
-    await mongoose.connect("mongodb+srv://vishalhuneria:0Zh6A7MSQALfxdGF@shopit.0zy3x.mongodb.net/shopit?retryWrites=true&w=majority&appName=shopit");
+    await mongoose.connect(
+      "mongodb+srv://vishal:12345@mern.r2r6z.mongodb.net/?retryWrites=true&w=majority&appName=mern"
+    );
 
     await Product.deleteMany();
     console.log("Products are deleted");
